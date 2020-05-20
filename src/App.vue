a13718436e7a92d5f3e98f766b06ab7eae92ce79
idealclover <idealclover@163.com>
2020-05-21 02:02:31 +0800

    :bug: fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -205,7 +205,7 @@ export default {
   methods: {
     open: function(link) {
       // window.location.href = link;
-      window.open(link, "_blank", "noopener,noreferrer,nofollow");
+      window.open(link);
     },
     share: function() {
       nativeShare.setShareData({
