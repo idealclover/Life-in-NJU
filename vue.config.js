f79bf133a5e1e7f551ecbc4b8e61506aea41eca2
1live <yfzhang2017@lzu.edu.cn>
2020-09-30 11:15:36 +0800

    修改内容

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
   pwa: {
-    name: "南哪指南",
+    name: "爱兰岛",
     themeColor: "#5b145c",
     workboxOptions: {
       skipWaiting: true
