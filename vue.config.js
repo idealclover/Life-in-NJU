c25e30b33016babfd25412d1fdf4e119423cc778
1live <yfzhang2017@lzu.edu.cn>
2020-10-06 16:15:34 +0800

    修改图片

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
   pwa: {
-    name: "life-under-lzu",
+    name: "爱兰岛",
     themeColor: "#5b145c",
     workboxOptions: {
       skipWaiting: true
