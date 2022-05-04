f11c19071f93c839c233c0ed02d74fe2fe0661a6
idealclover <idealclover@live.com>
2022-05-04 20:09:01 +0800

    :tada: happy 120!

--- src/App.vue
+++ src/App.vue
@@ -127,7 +127,7 @@
         class="text-center white--text"
         style="background-color: rgba(0,0,0,.2); flex: 0 1 auto"
         cols="12"
-      >
+        >祝南哪 120 周年生日快乐！<br />
         ©{{ new Date().getFullYear() }}
         <a
           href="https://idealclover.top"
@@ -157,28 +157,28 @@ import Clipboard from "clipboard";
 
 const dataUrl = "https://image.idealclover.cn/projects/Life-in-NJU/";
 const imgUrl =
-  dataUrl + "background/bg" + Math.floor(Math.random() * 10) + ".jpg";
-
+  dataUrl + "background/bg" + Math.floor(Math.random() * 24) + ".jpg";
+console.log(imgUrl);
 new Clipboard(".shareLink");
 Vue.use(Vuex);
 let nativeShare = new NativeShare();
 
 const store = new Vuex.Store({
   state: {
-    engineIndex: 0
+    engineIndex: 0,
   },
   mutations: {
     changeEngineIndex(state, num) {
       state.engineIndex = num;
-    }
+    },
   },
-  plugins: [createPersiste()]
+  plugins: [createPersiste()],
 });
 
 export default {
   name: "App",
   mounted() {
-    axios.get("./data.json").then(response => (this.data = response.data));
+    axios.get("./data.json").then((response) => (this.data = response.data));
   },
   data: () => ({
     data: null,
@@ -192,19 +192,19 @@ export default {
       {
         name: "baidu",
         url: "https://www.baidu.com/s?wd=",
-        src: dataUrl + "search/baidu-white.png"
+        src: dataUrl + "search/baidu-white.png",
       },
       {
         name: "bing",
         url: "https://cn.bing.com/search?q=",
-        src: dataUrl + "search/bing-white.png"
+        src: dataUrl + "search/bing-white.png",
       },
       {
         name: "google",
         url: "https://www.google.com/search?q=",
-        src: dataUrl + "search/google-white.png"
-      }
-    ]
+        src: dataUrl + "search/google-white.png",
+      },
+    ],
   }),
   methods: {
     open: function(link) {
@@ -219,7 +219,7 @@ export default {
         link: "https://nju.today",
         title: "南哪指南",
         desc: "南哪人的专属导航页！",
-        from: "@idealclover"
+        from: "@idealclover",
       });
 
       // 唤起浏览器原生分享组件(如果在微信中不会唤起，此时call方法只会设置文案。类似setShareData)
@@ -245,8 +245,8 @@ export default {
     showToast: function(text) {
       this.snackText = text;
       this.snackbar = true;
-    }
-  }
+    },
+  },
 };
 </script>
 
